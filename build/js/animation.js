bb029054ffe72e9ed47f9d63d0babf4cd40de6b6
DESKTOP-NDUN9JV\Nadya <nadia_degtiareva@rambler.ru>
2021-04-20 18:56:20 +0300

    анимации

--- build/js/animation.js
+++ build/js/animation.js
@@ -1,30 +1,44 @@
-const scroller = new LocomotiveScroll({
+const scroller = {
 	el: document.querySelector('[data-scroll-container]'),
-	smooth: true
-})
+	smooth: true,
+	getDirection: true,
+	reloadOnContextChange:true
+}
+
+const scroll = new LocomotiveScroll(scroller);
 
 gsap.registerPlugin(ScrollTrigger)
-
-scroller.on('scroll', ScrollTrigger.update)
-
-ScrollTrigger.scrollerProxy(
-	'.container-animation', {
-		scrollTop(value) {
-			return arguments.length ?
-				scroller.scrollTo(value, 0, 0) :
-				scroller.scroll.instance.scroll.y
-		},
-		getBoundingClientRect() {
-			return {
-				left: 0, top: 0,
-				width: window.innerWidth,
-				height: window.innerHeight
-			}
-		}
-	}
-)
-
-ScrollTrigger.addEventListener('refresh', () => scroller.update())
-
+scroll.on('scroll', ScrollTrigger.update)
+ScrollTrigger.addEventListener('refresh', () => scroll.update())
 ScrollTrigger.refresh()
 
+const header = document.getElementById('header');
+let hidden = false, visible = true;
+
+scroll.on('scroll',(instance)=> {
+	let headerHeight = header.getBoundingClientRect().height;
+	if (instance.direction === 'down' && visible) {
+		if (instance.scroll.y > headerHeight) {
+			header.classList.add('visible');
+			visible = false;
+		}
+	}
+	if(instance.direction === 'up' && !visible){
+		if(instance.scroll.y <= headerHeight){
+			header.classList.remove('visible');
+			visible = true;
+		}
+	}
+	if(instance.direction === 'down' && !hidden){
+		if(instance.scroll.y>(headerHeight+200)){
+			header.classList.remove('visible');
+			header.classList.add('hidden');
+			hidden = true;
+		}
+	}
+	if(instance.direction === 'up' && hidden){
+		header.classList.remove('hidden');
+		header.classList.add('visible');
+		hidden = false;
+	}
+});
